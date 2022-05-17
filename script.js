6ede475ab29c3a7d228bd3de661a6c32651277b8
dacsantos <72028806+dacsantos@users.noreply.github.com>
2022-05-18 00:18:37 +0100

    Mexi na tabela, nas cores e no script

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 window.onload = function() {
  
-
-    const loanForm = document.getElementById('calculate-bill-form')
-    loanForm.addEventListener('submit', function(e) {
+    const billForm = document.getElementById('calculate-bill-form')
+    billForm.addEventListener('submit', function(e) {
         e.preventDefault()
         const electricityUnits = document.getElementById('electricity-units').value;
         const billingPeriod = document.getElementById('billing-period').value;
@@ -15,15 +14,18 @@ window.onload = function() {
         totalLessVat = ((parseFloat(electricityUnits) * (parseFloat(flatRate/100))) + (parseFloat(billingPeriod) * (parseFloat(standardCharge/100))));
         totalPayable = parseFloat(totalLessVat) + parseFloat(totalLessVat)* vat/100;
 
-        document.getElementById('monthly-pay').textContent = "€ " + parseFloat(totalLessVat).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
+        document.getElementById('total-less-vat').textContent = "€ " + parseFloat(totalLessVat).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
             document.getElementById('total-pay').textContent = "€ " + parseFloat(totalPayable).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
+            document.getElementById('vat').textContent = "€ " + parseFloat(parseFloat(totalLessVat)* vat/100).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
             document.getElementById('table-title').style.display = 'block';
             document.getElementById('result').style.display = 'table';
             document.getElementById('reset-btn').style.display = 'block';
      })
-    loanForm.addEventListener('reset', function(e) {
-            document.getElementById('monthly-pay').textContent = ""
-            document.getElementById('total-pay').textContent = ""
+
+    billForm.addEventListener('reset', function(e) {
+            document.getElementById('total-less-vat').textContent = "";
+            document.getElementById('total-pay').textContent = "";
+            document.getElementById('vat').textContent = "";
             document.getElementById('table-title').style.display = 'none';
             document.getElementById('result').style.display = 'none';
             document.getElementById('reset-btn').style.display = 'none';
